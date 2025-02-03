6908a8e4afae800c1ea30c81d722c87f78d6906b
nginn1 <146990320+nginn1@users.noreply.github.com>
2025-02-03 13:35:06 -0500

    Add Base Project 1) Adding Eclipse project to /code/

---
+++ Code/JavaFX Sample/src/main/java/edu/westga/comp4420/javafx_sample/view/codebehind/MainWindow.java
@@ -0,0 +1,12 @@
+package edu.westga.comp4420.javafx_sample.view.codebehind;
+
+
+/**
+ * CodeBehind To Handle Processing for the MainWindow
+ *
+ * @author	Comp 4420
+ * @version Spring 2024
+ */
+public class MainWindow {   
+
+}
